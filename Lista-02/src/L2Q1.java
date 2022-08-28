253da0f348b64880a6f0458b92d78f2090952b97
Camila Belmont <camilabelmont.alu@gmail.com>
2022-08-28 20:35:11 -0300

    update

--- Lista-02/src/L2Q1.java
+++ Lista-02/src/L2Q1.java
@@ -9,15 +9,24 @@ public class L2Q1 {
         String fileName = input.nextLine();
 
         System.out.println("Agora digite a substring do arquivo:");
+        System.out.println("*Digite /stop/ para parar* \n");
         Scanner input2 = new Scanner(System.in);
         PrintStream ps= new PrintStream(fileName);
         
         while(input2.hasNextLine()) {
             ps.println(input2.nextLine());
-            if(input2.nextLine().equals("stop"))
+            if(input2.nextLine().equals("/stop/"))
                 break;
         }
 
+        InputStream is = new FileInputStream(fileName);
+        Scanner input3 = new Scanner(is);
+
+        while(input3.hasNextLine()) {
+            System.out.println(input3.nextLine());
+        }
+
+        input3.close();
         input2.close();
         input.close();
         ps.close();
