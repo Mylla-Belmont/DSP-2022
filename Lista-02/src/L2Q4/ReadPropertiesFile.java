4e6dde094286b0dce9b57301ccd6777b6771cf7f
Camila Belmont <camilabelmont.alu@gmail.com>
2022-08-29 20:43:22 -0300

    update

--- Lista-02/src/L2Q4/ReadPropertiesFile.java
+++ Lista-02/src/L2Q4/ReadPropertiesFile.java
@@ -1,5 +1,6 @@
 package L2Q4;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.Scanner;
@@ -8,16 +9,23 @@ public class ReadPropertiesFile {
     public static void main(String[] args) throws IOException {
 
         Properties prop = new Properties();
-        System.out.println("Digite o nome do arquivo:");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome do arquivo:");
         String input = scanner.nextLine();
 
-        prop.load(ReadPropertiesFile.class.getClassLoader()
-                .getResourceAsStream(input));
+        try {
+            prop.load(new FileInputStream(input));
+            String fileName = prop.getProperty("arquivo");
+            String firstLine = prop.getProperty("linha_inicial");
+            String endLine = prop.getProperty("linha_final");
+            
+            System.out.println(fileName);
+            System.out.println(firstLine);
+            System.out.println(endLine);
 
-        System.out.println(prop.getProperty("database"));
-        System.out.println(prop.getProperty("dbuser"));
-        System.out.println(prop.getProperty("dbpassword"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
         scanner.close();
     }
 }
--- Lista-02/src/L2Q4/WritePropertiesFile.java
+++ Lista-02/src/L2Q4/WritePropertiesFile.java
@@ -3,14 +3,27 @@ package L2Q4;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class WritePropertiesFile {
     public static void main(String[] args) throws IOException {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o nome do arquivo:");
+        String fileName = input.nextLine();
+
+        System.out.println("Digite a linha inicial de leitura:");
+        String firstLine = input.nextLine();
+
+        System.out.println("Digite a linha final de leitura:");
+        String endLine = input.nextLine();
+
         Properties prop = new Properties();
-        prop.setProperty("database", "localhost");
-        prop.setProperty("dbuser", "camila");
-        prop.setProperty("dbpassword", "creepypasta");
+        prop.setProperty("arquivo", fileName);
+        prop.setProperty("linha_inicial", firstLine);
+        prop.setProperty("linha_final", endLine);
         prop.store(new FileOutputStream("config.properties"), null);
+
+        input.close();
     }
 }
