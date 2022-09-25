13e290fdfefbc60a2269d0a3d0c5c467696d74d5
Camila Belmont <camilabelmont.alu@gmail.com>
2022-09-25 14:32:18 -0300

    update

--- trabalho_01/src/main/java/com/camila/TP_Q4.java
+++ trabalho_01/src/main/java/com/camila/TP_Q4.java
@@ -1,6 +1,16 @@
 package com.camila;
 
+import java.util.Scanner;
+
 public class TP_Q4 {
     public static void main(String[] args){
+        
+        System.out.println("Digite o nome do arquivo:");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        
+
+        scanner.close();
     }
 }
