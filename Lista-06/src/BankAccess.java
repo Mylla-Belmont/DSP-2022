a1a78ca9db8736e4af3bcc32573390afed45c120
Camila Belmont <camilabelmont.alu@gmail.com>
2022-10-01 18:29:53 -0300

    update

--- Lista-06/src/BankAccess.java
+++ Lista-06/src/BankAccess.java
@@ -11,10 +11,23 @@ public class BankAccess {
     public void insertAluno (int cpf, int matricula, String nome, String email, int telefone) {
         try {
             ConnectionPGSQL bank = connect();
-            String sql = "insert into aluno(id, cpf, matricula, nome, email, telefone)" + 
+            String sql = "insert into alunos(id, cpf, matricula, nome, email, telefone)" + 
                          "values(default, '" + cpf + "','" + matricula + "','" + nome + "','" + email + "','" + telefone + "')";
             bank.update(sql);
             System.out.println("\n" + nome + " adicionado com sucesso!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listAlunos () {
+        try {
+            ConnectionPGSQL bank = connect();
+            String sql = "select id, cpf, matricula, nome, email, telefone from alunos";
+            ResultSet result = bank.select(sql);
+            while (result.next()) {
+                    
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
