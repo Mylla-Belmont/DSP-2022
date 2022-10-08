e731cc90ab7087ca28abf25f52fa3c93edd2a70b
Camila Belmont <camilabelmont.alu@gmail.com>
2022-10-08 16:46:16 -0300

    update

--- Lista-07/src/main/java/lista_07/lista_07/Lista07Application.java
+++ Lista-07/src/main/java/lista_07/lista_07/Lista07Application.java
@@ -1,13 +1,13 @@
-package lista_07.demo;
+package lista_07.lista_07;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Lista07Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Lista07Application.class, args);
 	}
 
 }
--- Lista-07/src/test/java/lista_07/lista_07/Lista07ApplicationTests.java
+++ Lista-07/src/test/java/lista_07/lista_07/Lista07ApplicationTests.java
@@ -1,10 +1,10 @@
-package lista_07.demo;
+package lista_07.lista_07;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Lista07ApplicationTests {
 
 	@Test
 	void contextLoads() {
