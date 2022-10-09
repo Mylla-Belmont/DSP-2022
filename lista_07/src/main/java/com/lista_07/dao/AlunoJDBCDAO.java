c8ef0f2e9a0dd55ef716008c36f317fd3434618f
Camila Belmont <camilabelmont.alu@gmail.com>
2022-10-08 21:06:12 -0300

    update

--- lista_07/src/main/java/com/lista_07/dao/AlunoJDBCDAO.java
+++ lista_07/src/main/java/com/lista_07/dao/AlunoJDBCDAO.java
@@ -1,5 +1,7 @@
 package com.lista_07.dao;
 
+import java.sql.Connection;
+
 import com.lista_07.entidade.Aluno;
 
 public class AlunoJDBCDAO implements AlunoDAO {
@@ -7,7 +9,7 @@ public class AlunoJDBCDAO implements AlunoDAO {
     public AlunoJDBCDAO() { }
 
     public void salvar(Aluno aluno) {
-        
+        Connection con = null;
     }
 
     public void deletar(int id) {
